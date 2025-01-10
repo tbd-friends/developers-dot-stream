6bbfe6d7abd8b0286665bcaccda9bb6e38cc70ef
Terry Burns-Dyson <terrybd@gmail.com>
2025-01-10 08:42:55 -0500

    Initial Commit
    Working Apphost and basic Blazor Web App.

---
+++ src/Developers.Stream.Web/Developers.Stream.Web/Program.cs
@@ -0,0 +1,38 @@
+using Developers.Stream.Web.Client.Pages;
+using Developers.Stream.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Developers.Stream.Web.Client._Imports).Assembly);
+
+app.Run();
