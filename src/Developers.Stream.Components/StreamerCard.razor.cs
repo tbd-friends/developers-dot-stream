ec3ca389d6efc46fb04fe3af3a4d491a68ea02f1
Terry Burns-Dyson <terrybd@gmail.com>
2025-01-20 08:13:16 -0500

    Created RCL for components.
    Moved card to component in class library.

---
+++ src/Developers.Stream.Components/StreamerCard.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Developers.Stream.Components;
+
+public partial class StreamerCard : ComponentBase
+{
+    [Parameter] public bool IsLive { get; set; }
+    [Parameter] public string StreamerName { get; set; } = null!;
+    [Parameter] public string Description { get; set; } = null!;
+    [Parameter] public IEnumerable<string> Badges { get; set; } = new List<string>();
+
+    private string OnlineStatus => IsLive ? string.Empty : "offline";
+}
