5fecc98d3d4db6b39e126c9881169f3d20dd48d4
Terry Burns-Dyson <terrybd@gmail.com>
2025-02-03 09:03:45 -0500

    Create StreamerCards component for displaying streamercards and accept StreamerDto into StreamerCard for display. (Removed Badges for now)

--- src/Developers.Stream.Components/StreamerCard.razor.cs
+++ src/Developers.Stream.Components/StreamerCard.razor.cs
@@ -1,14 +1,10 @@
-﻿using Developers.Stream.Infrastructure.Contracts;
+﻿using Developers.Stream.Shared_Kernel.DataTransfer;
 using Microsoft.AspNetCore.Components;
 
 namespace Developers.Stream.Components;
 
-public partial class StreamerCard(IStreamerQuery query) : ComponentBase
+public partial class StreamerCard : ComponentBase
 {
-    [Parameter] public bool IsLive { get; set; }
-    [Parameter] public string StreamerName { get; set; } = null!;
-    [Parameter] public string Description { get; set; } = null!;
-    [Parameter] public IEnumerable<string> Badges { get; set; } = new List<string>();
+    [Parameter] public StreamerDto Streamer { get; set; } = null!;
 
-    private string OnlineStatus => IsLive ? string.Empty : "offline";
 }
