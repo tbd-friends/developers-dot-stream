07398aa47991fb8be36dc223269cdc68b6ad8ea6
Terry Burns-Dyson <terrybd@gmail.com>
2025-03-10 07:48:41 -0400

    Remove Url from channel and include the identifier on the Streamer record

---
+++ src/Developers.Stream.Application/Commands/RegisterChannelIntent.cs
@@ -0,0 +1,8 @@
+﻿using Developers.Stream.Shared_Kernel;
+
+namespace Developers.Stream.Application.Commands;
+
+public class RegisterChannelIntent
+{
+    public record Command(PlatformIdentifier Platform, string UserIdentifier, string State);
+}
