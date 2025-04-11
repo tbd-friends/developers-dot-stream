df64f17c833a7afdee31f10d815fea4f839d5e00
Terry Burns-Dyson <terrybd@gmail.com>
2025-04-11 07:38:33 -0400

    Define delegate for extracting preferred username from twitch id token
    Spec for finding unverified channels given a state string

---
+++ src/Developers.Stream.Application/Specifications/UnverifiedChannelByStateSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using Developers.Stream.Domain;
+
+namespace Developers.Stream.Application.Specifications;
+
+public sealed class UnverifiedChannelByStateSpec : Specification<Channel>, ISingleResultSpecification<Channel>
+{
+    public UnverifiedChannelByStateSpec(string state)
+    {
+        Query.Where(s => s.State == state && !s.IsVerified);
+    }
+}
