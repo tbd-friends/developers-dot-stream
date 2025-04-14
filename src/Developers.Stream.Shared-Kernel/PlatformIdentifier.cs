f8698c3c397e78f2f3ac86156bcb16bf65360589
Terry Burns-Dyson <terrybd@gmail.com>
2025-04-14 08:12:19 -0400

    Keep platform url close to platform identifier

--- src/Developers.Stream.Shared-Kernel/PlatformIdentifier.cs
+++ src/Developers.Stream.Shared-Kernel/PlatformIdentifier.cs
@@ -4,22 +4,24 @@ namespace Developers.Stream.Shared_Kernel;
 
 public class PlatformIdentifier
 {
-    public static PlatformIdentifier Twitch = new("Twitch");
-    public static PlatformIdentifier YouTube = new("YouTube");
-    public static PlatformIdentifier Kick = new("Kick");
+    public static PlatformIdentifier Twitch = new("Twitch", "https://twitch.tv/");
+    public static PlatformIdentifier YouTube = new("YouTube", "https://www.youtube.com/@");
+    public static PlatformIdentifier Kick = new("Kick", "https://www.kick.tv/");
 
     public string Identifier { get; }
+    public string Address { get; }
 
     public static readonly PlatformIdentifier?[] Platforms = typeof(PlatformIdentifier)
         .GetFields(BindingFlags.Public | BindingFlags.Static)
         .Where(f => f.FieldType == typeof(PlatformIdentifier))
         .Select(f => f.GetValue(null) as PlatformIdentifier)
-        .Where(f => f is not null) 
+        .Where(f => f is not null)
         .ToArray();
 
-    private PlatformIdentifier(string identifier)
+    private PlatformIdentifier(string identifier, string siteAddress)
     {
         Identifier = identifier;
+        Address = siteAddress;
     }
 
     public static implicit operator string(PlatformIdentifier platformIdentifier)
