45ffc986fc42f03a0e476ceb0c0822d3e3957440
Terry Burns-Dyson <terrybd@gmail.com>
2025-05-09 08:44:48 -0400

    Create control for generate api key for streamer

---
+++ src/Developers.Stream.Components/Controls/StreamerApiKey.razor.cs
@@ -0,0 +1,22 @@
+﻿using Developers.Stream.Infrastructure.Contracts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Developers.Stream.Components.Controls;
+
+public partial class StreamerApiKey(
+    AuthenticationStateProvider stateProvider,
+    IStreamerProfileService profileService) : ComponentBase
+{
+    public string ApiKey { get; set; } = string.Empty;
+
+    private async Task GenerateApiKey()
+    {
+        var userState = await stateProvider.GetAuthenticationStateAsync();
+
+        var userId = Guid.Parse(userState.User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+        ApiKey = await profileService.GenerateApiKey(userId, CancellationToken.None);
+    }
+}
