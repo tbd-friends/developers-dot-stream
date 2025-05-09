2c4b8bff852985a4637815ad323c479cf5df511c
Terry Burns-Dyson <terrybd@gmail.com>
2025-05-09 08:07:23 -0400

    Create Command/Handler for registering a new api key, do not allow multiple keys for a user

--- src/Developers.Stream.Infrastructure/ApplicationRepository.cs
+++ src/Developers.Stream.Infrastructure/ApplicationRepository.cs
@@ -4,6 +4,4 @@ using Developers.Stream.Infrastructure.Contracts;
 namespace Developers.Stream.Infrastructure;
 
 public class ApplicationRepository<TEntity>(ApplicationDbContext context) : Repository<TEntity>(context)
-    where TEntity : class
-{
-}
+    where TEntity : class;
